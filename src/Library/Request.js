755333f30297e7341358314a15ef96bc60371cab
ryo4004 <kato@ryo4004.net>
2021-02-23 08:47:10 +0900

    環境変数を追加

--- src/Library/Request.js
+++ src/Library/Request.js
@@ -1,9 +1,14 @@
 import request from 'superagent'
+import process from 'process'
+
+const developmentUrl = 'http://localhost:3001'
+const productionUrl = 'https://app.osaifu.zatsuzen.com'
+const baseUrl = process.env.REACT_APP_MODE === 'production' ? productionUrl : developmentUrl
 
 export function post(url, send) {
   return new Promise((resolve, reject) => {
     request
-      .post(url)
+      .post(baseUrl + url)
       .type('form')
       .send(send)
       .end((err, response) => {
